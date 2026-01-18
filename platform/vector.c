78d6c283aba5363c577755a47c41dc08e8e7fbff
Jibril <128831088+jibril04Afaa@users.noreply.github.com>
2026-01-18 15:59:19 -0500

    vector table

--- platform/vector.c
+++ platform/vector.c
@@ -1,26 +1,16 @@
 #include <stdint.h>
 
-static void noop(void) {
-  while (1) { }
-}
 
-void reset(void);                 // defined in reset.c
-extern unsigned long _stack_top;  // from linker script
+/* Interrupt handling */
 
-__attribute__((section(".vector_table")))
-void (*const vector_table[16 + 96])(void) = {
-  (void (*)(void))(&_stack_top),  // Initial stack pointer
-  reset,                          // Reset
-  noop,                           // NMI
-  noop,                           // HardFault
-  noop,                           // MemManage
-  noop,                           // BusFault
-  noop,                           // UsageFault
-  0, 0, 0, 0,                     // Reserved
-  noop,                           // SVCall
-  noop,                           // DebugMon
-  0,                              // Reserved
-  noop,                           // PendSV
-  noop,                           // SysTick
-  [16 ... (16 + 96 - 1)] = noop
+extern uint32_t _stack_top;
+extern void Reset_Handler(void);
+extern void Default_Handler(void);
+
+__attribute__((section(".vectors")))
+void (* const vector_table[])(void) = {
+  (void (*)(void))(&_stack_top), // initial Stack Pointer
+  Reset_Handler, // reset
+  Default_Handler, // NMI
+  Default_Handler, // HardFault
 };
